8fcf0f730cf644abf57ebc35ace9fe0fae754cee
Alberto Vecchi <Alberto Vecchi>
2017-01-21 12:08:37 +0100

    Add guesses in guessing

--- guessingNumbers.java
+++ guessingNumbers.java
@@ -4,23 +4,30 @@ import java.util.Scanner;
 
 public class GuessingNumberGame {
 
-
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int numberDrawn = drawNumber();
-
+        
         // program your solution here. Do not touch the above lines!
         System.out.println("Guess a number: ");
+        int guesses = 1;
+        //int num = Integer.parseInt(reader.nextLine());
+        while(true){
+        
         int num = Integer.parseInt(reader.nextLine());
-
         if (num == numberDrawn) {
             System.out.println("Congratulations, your guess is correct!");
-
+            break;
         } else if (num < numberDrawn) {
-            System.out.println("The number is greater");
+            System.out.println("The number is greater, guesses made: " + guesses);
         } else if (num > numberDrawn) {
-            System.out.println("The number is lesser");
+            System.out.println("The number is lesser, guesses made: " + guesses);
         }
+        guesses++;
+    }
+        //System.out.println("Congratulations, your guess is correct!");
+
+        
 
     }
 
